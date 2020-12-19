0ad648985421e6fae9fb45ad62ac7a062937abde
Righson <ya-fhh@yandex.ru>
2020-12-20 00:52:54 +0300

    feature: test row win

--- src/ticktoe/core.clj
+++ src/ticktoe/core.clj
@@ -1,17 +1,27 @@
 (ns ticktoe.core
   (:gen-class))
 
+(defn reduce-board [f board]
+  (reduce #(and %1 (f %2)) true board))
+
+(defn test-row-win [row-cap board]
+  (loop [r (* row-cap row-cap)]
+    (let [next-row-cap (- r row-cap)
+          test-res (reduce-board #(bit-test board %) (range next-row-cap r))]
+      (if (and (> r row-cap) (not test-res))
+        (recur (- r row-cap))
+        test-res))))
 
 (defn make-move
   "Делает ход на доску board игрока player производя xor с текущим состояние доски"
-  [boards, row-cap, player, x, y]
+  [row-cap boards player x y]
   (let [s (+ (* row-cap x) y)
         board (get boards player)]
     (if (reduce #(and %1 (bit-test %2 s)) true boards) ;; (bit-test board s)
       (list false boards)
       (list true (assoc boards player (bit-xor board (bit-shift-left 1 s)))))))
 
-(defn cons-board [boards row-cap players]
+(defn cons-board [row-cap boards players]
   (loop [p-idx 0
          idx 0
          res ""]
@@ -26,8 +36,8 @@
               (recur (inc p-idx) idx res))))
         res))))
 
-(defn draw-board [boards row-cap players]
-  (-> (cons-board boards row-cap players)
+(defn draw-board [row-cap boards players]
+  (-> (cons-board row-cap boards players)
       (println)))
 
 
