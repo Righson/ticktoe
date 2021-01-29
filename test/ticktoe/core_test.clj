ff314ac5158cdec715697a3a20f52fd03fdedc37
Righson <ya-fhh@yandex.ru>
2021-01-30 01:01:34 +0300

    testing: add test for test-row-win

--- test/ticktoe/core_test.clj
+++ test/ticktoe/core_test.clj
@@ -13,3 +13,13 @@
     (is (= [1 0] (first (rest (make-move 3 [0 0] 0 0))))))
   (testing "make-move should return false if move is not available"
     (is (not (first (make-move 3 [1 0] 1 0))))))
+
+(deftest test-test-row-win
+  (testing "test-row-win return true if find 111 in mask"
+    (is (test-row-win 3 7  ))
+    (is (test-row-win 3 56 ))
+    (is (test-row-win 3 448)))
+  (testing "test-row-win return false if don't find 111 in mask"
+    (is (not (test-row-win 3 5  )))
+    (is (not (test-row-win 3 24 )))
+    (is (not (test-row-win 3 320)))))
